3f9bbe8f78a58d70e34621042b52edd5127e75bf
mohdhanif1234 <hanif.topia@gmail.com>
2022-12-10 21:06:19 +0530

    Polymorphism assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+
+    // creating a Product class
+    public static class Product{
+
+        // Assignment to test the learning of Polymorphism (Method Overloading) in Java
+        public int product(int x, int y){
+            return x * y;
+        }
+
+        public int product(int x, int y, int z){
+            return x * y * z;
+        }
+
+        public double product(double x, double y){
+            return x * y;
+        }
+    }
+    public static void main(String[] args){
+        Product p = new Product(); // creating object of Product class
+        System.out.println(p.product(2,5));
+        System.out.println(p.product(2,3,4));
+        System.out.println(p.product(1.2, 6.2));
+    }
 }
